da7054f42a2595ad0c7b2f3b4525e3eec55e1f27
Cristhian Sánchez <csanchezc5@est.ups.edu.ec>
2024-06-22 12:49:16 -0500

    Taller

---
+++ Talleres/script.js
@@ -0,0 +1,18 @@
+var contador = 1 
+function cargar_elemento() {
+    var cointainer = document.getElementbyId("container")
+    var div = document.createElement("div")
+    div.className= "item.box"
+    var texto = document.createTextNode("Item " + contador)
+    contador++
+    div.appendChild(texto)
+
+    content.appendChild(div)
+
+}
+
+function eliminar_elemento() {
+    var content= document.getElementbyId("content")
+    content.removeChild(content.lastElementChild)
+    contador--
+}
